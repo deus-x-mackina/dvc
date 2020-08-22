183364695980f1cbfa28b956ec7b873a1afa2ce8
Danny Mack <drmterps@yahoo.com>
2020-08-22 15:48:06 -0400

    Tweak dvcd script

--- bin/dvcd.sh
+++ bin/dvcd.sh
@@ -2,12 +2,13 @@
 # For Mac users, this may be ~/.zshrc or ~/.bash_profile.
 # For Linux users, this may be ~/.bashrc or ~/.profile.
 # You can execute `dvcd` to change into DVC's managed directory,
-# or `dvcd <user>/<repo>` to change into a specific repository. 
+# or `dvcd <account>` or `dvcd <account>/<repo>` to change into a specific
+# account folder or repo belonging to said account.
 function dvcd() {
-    if /usr/bin/which dvc >/dev/null; then
-        cd $(dvc --prefix)
-        if [[ $1 ]]; then
-            cd $1
+    if command -v dvc >/dev/null; then
+        cd "$(dvc --prefix)" || (echo "Could not change into managed directory." ; exit 1)
+        if [[ -n $1 ]]; then
+            cd "$1" || (echo "Could not change into subdirectory: $1." ; exit 2)
         fi
     else
         echo "DVC is not installed."
