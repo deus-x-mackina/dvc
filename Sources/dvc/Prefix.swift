1728509465b7e43db92ee5250569609722a36c4d
Danny Mack <drmterps@yahoo.com>
2020-09-23 22:49:07 -0400

    Add abstract

--- Sources/dvc/Prefix.swift
+++ Sources/dvc/Prefix.swift
@@ -1,6 +1,10 @@
 import ArgumentParser
 
 struct Prefix: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        abstract: "Print the path of the directory that DVC manages."
+    )
+
     func run() throws {
         print(MANAGED_PATH)
     }
